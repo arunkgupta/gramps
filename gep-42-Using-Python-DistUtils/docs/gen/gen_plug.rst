e5c168efab29f33bd8491089a49dbd0f1d9075a3
robhealey1 <robhealey1@27f82ba3-fc0d-0410-9f6a-8f6d5e09c20a>
2012-03-24 17:46:41 +0000

    git-svn-id: https://gramps.svn.sourceforge.net/svnroot/gramps/branches/geps@19136 27f82ba3-fc0d-0410-9f6a-8f6d5e09c20a

---
+++ gep-42-Using-Python-DistUtils/docs/gen/gen_plug.rst
@@ -0,0 +1,241 @@
+############################
+The :mod:`gen.plug` Module
+############################
+
+.. automodule:: gen.plug
+
+Plugin
+====================================
+.. automodule:: gen.plug._plugin
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+BaseManager
+====================================
+.. automodule:: gen.plug._manager
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Export
+====================================
+.. automodule:: gen.plug._export
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Gramplet
+====================================
+.. automodule:: gen.plug._gramplet
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Import
+====================================
+.. automodule:: gen.plug._import
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Menu
+====================================
+.. automodule:: gen.plug.menu._menu
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._boolean
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._booleanlist
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._color
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._destination
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._enumeratedlist
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._family
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._filter
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._media
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._note
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._number
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._option
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._person
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._personlist
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._placelist
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._string
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._style
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._surnamecolor
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.menu._text
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+*****************************
+Options
+*****************************
+.. automodule:: gen.plug._options
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Reports
+====================================
+.. automodule:: gen.plug.report._options
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.report._bibliography
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.report._constants
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.report.endnotes
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.report._paper
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.report._reportbase
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.report.utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+*****************************
+Docgen
+*****************************
+.. automodule:: gen.plug._docgenplugin
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+*****************************
+Generators
+*****************************
+.. automodule:: gen.plug.docgen.basedoc
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docgen.drawdoc
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docgen.fontscale
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docgen.fontstyle
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docgen.graphdoc
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docgen.graphicstyle
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docgen.paperstyle
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docgen.paragraphstyle
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docgen.stylesheet
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docgen.tablestyle
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docgen.textdoc
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+*****************************
+Doc Backend
+*****************************
+.. automodule:: gen.plug.docbackend.cairobackend
+   :members:
+   :undoc-members:
+   :show-inheritance:
+.. automodule:: gen.plug.docbackend.docbackend
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Register
+====================================
+.. automodule:: gen.plug._pluginreg
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Utils
+====================================
+.. automodule:: gen.plug.utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
---
+++ gep-42-Using-Python-DistUtils/src/gui/editors/displaytabs/backreflist.py
@@ -0,0 +1,190 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2000-2007  Donald N. Allingham
+# Copyright (C) 2009-2011  Gary Burton
+# Copyright (C) 2011       Tim G L Lyons
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+# $Id$
+
+#-------------------------------------------------------------------------
+#
+# Python classes
+#
+#-------------------------------------------------------------------------
+from gen.ggettext import gettext as _
+
+#-------------------------------------------------------------------------
+#
+# GTK libraries
+#
+#-------------------------------------------------------------------------
+import gtk
+
+#-------------------------------------------------------------------------
+#
+# GRAMPS classes
+#
+#-------------------------------------------------------------------------
+import Errors
+from gui.widgets import SimpleButton
+from embeddedlist import EmbeddedList
+
+#-------------------------------------------------------------------------
+#
+# BackRefList
+#
+#-------------------------------------------------------------------------
+class BackRefList(EmbeddedList):
+
+    _HANDLE_COL = 3
+
+    #index = column in model. Value =
+    #  (name, sortcol in model, width, markup/text, weigth_col
+    _column_names = [
+        (_('Type'), 0, 100, 0, -1),
+        (_('ID'),  1,  75, 0, -1),
+        (_('Name'), 2, 250, 0, -1),
+        ]
+    
+    def __init__(self, dbstate, uistate, track, obj, refmodel, callback=None):
+        self.obj = obj
+        EmbeddedList.__init__(self, dbstate, uistate, track, 
+                              _('_References'), refmodel)
+        self._callback = callback
+        self.connectid = self.model.connect('row-inserted', self.update_label)
+        self.track_ref_for_deletion("model")
+
+    def update_label(self, *obj):
+        if self.model.count > 0:
+            self._set_label()
+        if self._callback and self.model.count > 1:
+            self._callback()
+
+    def right_click(self, obj, event):
+        return
+
+    def _cleanup_local_connects(self):
+        self.model.disconnect(self.connectid)
+
+    def _cleanup_on_exit(self):
+        # model may be destroyed already in closing managedwindow
+        if hasattr(self, 'model'):
+            self.model.destroy()
+
+    def is_empty(self):
+        return self.model.count == 0
+
+    def _create_buttons(self, share=False, move=False, jump=False, top_label=None):
+        """
+        Create a button box consisting of one button: Edit. 
+        This button box is then appended hbox (self).
+        Method has signature of, and overrides create_buttons from _ButtonTab.py
+        """
+        self.edit_btn = SimpleButton(gtk.STOCK_EDIT, self.edit_button_clicked)
+        self.edit_btn.set_tooltip_text(_('Edit reference'))
+
+        hbox = gtk.HBox()
+        hbox.set_spacing(6)
+        hbox.pack_start(self.edit_btn, False)
+        hbox.show_all()
+        self.pack_start(hbox, False)
+        
+        self.add_btn = None
+        self.del_btn = None
+
+        self.track_ref_for_deletion("edit_btn")
+        self.track_ref_for_deletion("add_btn")
+        self.track_ref_for_deletion("del_btn")
+
+    def _selection_changed(self, obj=None):
+        if self.dirty_selection:
+            return
+        if self.get_selected():
+            self.edit_btn.set_sensitive(True)
+        else:
+            self.edit_btn.set_sensitive(False)
+
+    def get_data(self):
+        return self.obj
+
+    def column_order(self):
+        return ((1, 0), (1, 1), (1, 2))
+
+    def find_node(self):
+        (model, node) = self.selection.get_selected()
+        try:
+            return (model.get_value(node, 4), model.get_value(node, 3))
+        except:
+            return (None, None)
+    
+    def edit_button_clicked(self, obj):
+        
+        from gui.editors import EditEvent, EditPerson, EditFamily, EditSource, \
+                                EditPlace, EditMedia, EditRepository, \
+                                EditCitation
+
+        (reftype, ref) = self.find_node()
+        if reftype == 'Person':
+            try:
+                person = self.dbstate.db.get_person_from_handle(ref)
+                EditPerson(self.dbstate, self.uistate, [], person)
+            except Errors.WindowActiveError:
+                pass
+        elif reftype == 'Family':
+            try:
+                family = self.dbstate.db.get_family_from_handle(ref)
+                EditFamily(self.dbstate, self.uistate, [], family)
+            except Errors.WindowActiveError:
+                pass
+        elif reftype == 'Source':
+            try:
+                source = self.dbstate.db.get_source_from_handle(ref)
+                EditSource(self.dbstate, self.uistate, [], source)
+            except Errors.WindowActiveError:
+                pass
+        elif reftype == 'Citation':
+            try:
+                citation = self.dbstate.db.get_citation_from_handle(ref)
+                EditCitation(self.dbstate, self.uistate, [], citation)
+            except Errors.WindowActiveError:
+                pass
+        elif reftype == 'Place':
+            try:
+                place = self.dbstate.db.get_place_from_handle(ref)
+                EditPlace(self.dbstate, self.uistate, [], place)
+            except Errors.WindowActiveError:
+                pass
+        elif reftype == 'MediaObject':
+            try:
+                obj = self.dbstate.db.get_object_from_handle(ref)
+                EditMedia(self.dbstate, self.uistate, [], obj)
+            except Errors.WindowActiveError:
+                pass
+        elif reftype == 'Event':
+            try:
+                event = self.dbstate.db.get_event_from_handle(ref)
+                EditEvent(self.dbstate, self.uistate, [], event)
+            except Errors.WindowActiveError:
+                pass
+        elif reftype == 'Repository':
+            try:
+                repo = self.dbstate.db.get_repository_from_handle(ref)
+                EditRepository(self.dbstate, self.uistate, [], repo)
+            except Errors.WindowActiveError:
+                pass
