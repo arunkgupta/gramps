1e057e4935ce5ff5a9fd43fce8dc1317d0bcfacf
robhealey1 <robhealey1@27f82ba3-fc0d-0410-9f6a-8f6d5e09c20a>
2012-04-12 05:43:08 +0000

    Creating of setup.cfg is going well.  I can: * pysetup run build, * pysetup run sdist successfully now, but still working on getting the complete ecosystem.
    git-svn-id: https://gramps.svn.sourceforge.net/svnroot/gramps/branches/geps@19302 27f82ba3-fc0d-0410-9f6a-8f6d5e09c20a

--- gep-026-replace-make/create_setup_cfg.py
+++ gep-026-replace-make/create_setup_cfg.py
@@ -25,15 +25,9 @@
 # ***********************************************
 # Python Modules
 # ***********************************************
-import os
+import os, sys
 import glob, shutil
 import codecs
-
-#------------------------------------------------
-#        Distutils/ Distutils2 modules
-#------------------------------------------------
-from distutils2 import logger
-from distutils2.util import find_packages
 
 #------------------------------------------------
 #        Gramps modules
@@ -45,17 +39,10 @@ from classifiers import all_classifiers
 #------------------------------------------------
 _FILENAME = 'setup.cfg'
 
-# make absolute sure that this file exists?
-gramps_sh_in   = 'gramps.sh.in'
-gramps_sh_file = 'gramps.sh'
-if (os.path.exists(gramps_sh_in) and not os.path.exists(gramps_sh_file)):
-    shutil.copy(gramps_sh_in, gramps_sh_file)
-
-# make absolute sure that this file exists?
-const_py_in   = os.path.join('gramps', 'const.py.in')
-const_py_file = os.path.join('gramps', 'const.py')
-if (os.path.exists(const_py_in) and not os.path.exists(const_py_file)):
-    shutil.copy(const_py_in, const_py_file)
+curr_ver = 'You currently have Python-%d.%d.%d installed.' % sys.version_info[0:3]
+if sys.version_info < (2, 6):
+    sys.exit('Error: Python-%d.%d.%d or newer is required.\n %s\n'
+             % (2, 6, 0, curr_ver))
 
 '''
     This will create the setup.cfg for use in setup.py.
@@ -67,51 +54,49 @@ $ python create_setup_cfg.py ...
 '''
 class CreateSetup(object):
     def __init__(self):
-        self.data = {'name': 'gramps',
-                     'version': '3.5.0',
-                     'Home-page' : 'http://gramps-project.org/',
-                     'download_url' : '',
-                     'author' : '', 
-                     'author_email' : '',
-                     'maintainter' : '',
-                     'maintainer_email' : '',
-                     'classifiers': [x for x in all_classifiers.split('\n')],
-                     'keywords' : [],
-                     'summary' : '',
-                     'description' : '',
-                     'license' : '',
-                     'platforms': [],
-                     'packages': sorted(find_packages()),
-                     'package_data' : dict(),
-                     'py_modules': [],
-                     'resources': [],
-                     'extra_files': [],
-                     'data_files' : [],
-                     'scripts': [],
-                     }
-
-        self.data['download_url'] = 'http://gramps-project.org/download/'
-
-        self.data['author'] = 'Donald N. Allingham'
-
-        self.data['author_email'] = 'don@gramps-project.org'
-
-        self.data['maintainer'] = 'Gramps Development Team'
-
-        self.data['maintainer_email'] = 'benny.malengier@gmail.com'
+        self.data = {
+            'name'               : 'gramps',
+            'version'            : '3.5.0',
+             'home-page'         : 'http://gramps-project.org/',
+             'download-url'      : 'http://gramps-project.org/download/',
+             'author'            : 'Donald N. Allingham',
+            'author-email'       : 'don@gramps-project.org',
+            'maintainer'         : 'Gramps Development Team',
+            'maintainer-email'   : 'benny.malengier@gmail.com',
+            'summary'            : '',
+            'description'        : '',
+            'classifier'         : [x for x in all_classifiers.split('\n')],
+            'keywords'           : [],
+            'license'            : 'GPL v2 or greater',
+            'platform'           : [],
+            'requires-dist'      : [],
+            'provides-dist'      : [],
+            'obsoletes-dist'     : [],
+            'requires-externals' : [], 
+            'requires-python'    : '>= 2.6',
+            'packages'           : [],
+            'package_data'       : dict(),
+            'modules'            : [],
+            'resources'          : [],
+            'extra_files'        : [],
+            'data_files'         : dict(),
+            'scripts'            : [],
+         }
 
         self.data['summary'] = 'Gramps (Genealogical Research and Analysis Management Programming System)'
 
         self.data['description'] = '''gramps (Genealogical Research and Analysis Management Programming
-        System) is a GNOME based genealogy program supporting a Python based plugin system.
-        '''
+        System) is a GNOME based genealogy program supporting a Python based plugin system.'''
 
-        self.data['license'] = 'GPL v2 or greater'
+        self.data['project-url'] = [
+            'Repository, https://gramps.svn.sourceforge.net/svnroot/gramps/trunk',
+            'Wiki, http://www.gramps-project.org/wiki/index.php?title=Main_page',
+            'Bug tracker, http://bugs.gramps-project.org']
 
-        self.data['platforms'] = [
+        self.data['platform'] = [
             'Linux',
             'FreeBSD',
-            'Mac OSX',
+            'MacOS',
             'Windows']
 
         self.data['keywords'] = [
@@ -125,51 +110,25 @@ class CreateSetup(object):
             'Family-tree',
             'GEDCOM']
 
-        all_files = {
-            'gramps' : [
-                '*.py',
-                'DateHandler/*.py',
-                'docgen/*.py',
-                'Filters/*.py',
-                'Filters/*/*.py',
-                'Filters/Rules/*/*.py',
-                'GrampsLocale/*.py',
-                'GrampsLogger/*.py',
-                'Merge/*.py',
-                'Simple/*.py',
-                'TestPlan.txt',
-                'test/*.py'],
-            'gramps.cli': [
-                '*.py',
-                'plug/*.py'],
+        # these packages have a __init__.py within them, so they are pure python packages,
+        # the remaining four are data or extra files...
+        # this list has been taken from, 
+        self.data['packages'] = [
+            'gramps',
+            'gramps.cli',
+            'gramps.gen',
+            'gramps.gui',
+            'gramps.webapp']
+
+        # these files coe from directories/ packages that do NOT have an __init__.py file...
+        self.data['package_data'] = {
             'gramps.data': [
                 '*.txt',
                 '*.xml'],
-            'gramps.gen': [
-                '*.py',
-                'db/*.py',
-                'display/*.py',
-                'lib/*.py',
-                'mime/*.py',
-                'plug/*.py',
-                'plug/*/*.py',
-                'proxy/*.py',
-                'test/*.py',
-                'utils/*.py'],
             'gramps.glade': [
                 '*.glade',
                 'glade/catalog/*.py',
                 'catalog/*.xml'],
-            'gramps.gui': [
-                '*.py',
-                'editors/*.py',
-                'editors/*/*.py',
-                'plug/*.py',
-                'plug/*/*.py',
-                'selectors/*.py',
-                'views/*.py',
-                'views/treemodels/*.py',
-                'widgets/*.py'],
             'gramps.images': [
                 '*/*.png',
                  '*/*.svg',
@@ -188,140 +147,94 @@ class CreateSetup(object):
                 'webstuff/images/*.gif',            
                 '*.glade',
                 '*/*.glade'],
-                'gramps.webapp': [
-                '*.py',
-                'webstuff/css/*.css',
-                'webstuff/images/*.svg',
-                'webstuff/images/*.png',
-                'webstuff/images/*.gif',
-                'grampsdb/fixtures/initial_data.json',
-                '*/*.py',
-                'sqlite.db',
-                'grampsdb/*.py',
-                'fixtures/initial_data.json',
-                'templatetags/*py'],
         }
-
-        file_data = dict((section, list()) for section in all_files
-                if section not in ['gramps.data', 'gramps.glade', 'gramps.images', 'gramps.plugins'])
-
-        extra_data = dict((section, list()) for section in all_files
-                if section in ['gramps.data', 'gramps.glade', 'gramps.images', 'gramps.plugins'])
-
-        for section in all_files:
-            directory = section
-            if '.' in directory:
-                directory = '/'.join(directory.split('.'))
-            tmp_filenames = []
-            for file_match in all_files[section]:
-                tmp_filenames.append([glob.glob(os.path.join(directory, file_match))])
-                if section in file_data:
-                    file_data[section] = tmp_filenames
-                else:
-                    extra_data[section] = tmp_filenames
-        #self.data['py_modules'] = file_data
-        self.data['extra_files'] = [
-            'debian/*',
-            'docs/*',
-            'docs/*/*',
-            'help/*',
-            'mac/*',
-            'po/*',
-            'po/*/*',
-            'test/*',
-            'test/*/*',
-            'windows/*',
-            'windows/*/*']
 
         self.data['scripts'] = ['gramps.sh']
 
-        self.data['requires_dist'] = [
+#        self.data['data_files'] = {
+#            '../debian'  : ['*.*'],
+#            '../data'    : ['*.*'],
+#            '../docs'    : ['*.*'],
+#            '../help'    : ['*.*'],
+#            '../mac'     : ['*.*'],
+#            '../po'      : ['*.*'],
+#            '../test'    : ['*.*'],
+#            '../windows' : ['*.*'],
+#        }
+
+        self.data['requires-dist'] = [
             'pygtk2',
             'pycairo',
             'pygobject2']
 
         self.data['resources'] = [
-            'example/**/*.* = {purelib}/gramps',
-            'gramps/glade/**/*.* = {purelib}',
-            'gramps/images/**/*.* = {purelib}',
-            'gramps/plugins/**/*.* = {purelib}',
-            'data/ gramps.desktop = {data}/share/applications',
-            'gramps/images/ gramps.png = {icon}',
-            'data/ gramps.xml = {data}/share/mime/packages',
-            'data/ gramps.mime = {data}/share/mime-info',
-            'data/ gramps.keys = {data}/share/mime-info',
-            'data/ *.png = {data}/share/icons/gnome/48x48/mimetypes',
-            'data/ *.svg = {data}/share/icons/gnome/scalable/mimetypes',
-            'data/man/ gramps.1.in = {man}/man1',
-            'data/man/cs/ gramps.1.in = {man}/cs/man1',
-            'data/man/fr/ gramps.1.in = {man}/fr/man1',
-            'data/man/nl/ gramps.1.in = {man}/nl/man1',
-            'data/man/pl/ gramps.1.in = {man}/pl/man1',
-            'data/man/sv/ gramps.1.in = {man}/sv/man1',
-            'COPYING = {doc}',
-            'FAQ = {doc}',
-            'INSTALL = {doc}',
-            'NEWS = {doc}',
-            'README = {doc}',
-            'TODO = {doc}']
+            '../example/* = {purelib}/gramps',
+            'glade/* = {purelib}',
+            'images/* = {purelib}',
+            'plugins/* = {purelib}',
+            '../data/gramps.desktop = {data}/share/applications',
+            'images/gramps.png = {icon}',
+            '../data/gramps.xml = {data}/share/mime/packages',
+            '../data/gramps.mime = {data}/share/mime-info',
+            '../data/gramps.keys = {data}/share/mime-info',
+            '../data/*.png = {data}/share/icons/gnome/48x48/mimetypes',
+            '../data/*.svg = {data}/share/icons/gnome/scalable/mimetypes',
+            '../data/man/gramps.1.in = {man}/man1',
+            '../data/man/cs/gramps.1.in = {man}/cs/man1',
+            '../data/man/fr/gramps.1.in = {man}/fr/man1',
+            '../data/man/nl/gramps.1.in = {man}/nl/man1',
+            '../data/man/pl/gramps.1.in = {man}/pl/man1',
+            '../data/man/sv/gramps.1.in = {man}/sv/man1',
+            '../COPYING = {doc}',
+            '../FAQ = {doc}',
+            '../INSTALL = {doc}',
+            '../NEWS = {doc}',
+            '../README = {doc}',
+            '../TODO = {doc}']
+
+    def multi_string(self, data):
+        return (u''.join('    %s\n' % val
+                         for val in data).lstrip())
 
     def main(self):
         if os.path.exists(_FILENAME):
             if os.path.exists('%s.old' % _FILENAME):
-                message = ("ERROR: %(name)s.old backup exists, please check "
-                           "that current %(name)s is correct and remove "
-                           "%(name)s.old" % {'name': _FILENAME})
-                logger.error(message)
-                return
+                os.remove('%s.old' % _FILENAME)
             else:
                 shutil.move(_FILENAME, '%s.old' % _FILENAME)
 
         try: 
             fp = codecs.open(_FILENAME, 'w', encoding='utf-8')
-        except IOError:
-            print "ERROR: Failed to open file."
-            return
+        except (IOError, OSError):
+            sys.exit('ERROR:  Failed to write %s file' % _FILENAME)
             
         fp.write(u'[metadata]\n')
-        # TODO use metadata module instead of hard-coding field-specific
-        # behavior here
 
-        # simple string entries
-        for name in ('name', 'version', 'summary', 'download_url'):
+        for name in ('name', 'version', 'summary', 'description', 'home-page', 'download-url',
+                     'author', 'author-email', 'maintainer', 'maintainer-email'):
             fp.write(u'%s = %s\n' % (name, self.data.get(name, 'UNKNOWN')))
 
-        # optional string entries
-        if ('keywords' in self.data and self.data['keywords']):
-            # XXX shoud use comma to separate, not space
-            fp.write(u'keywords = %s\n' % '\n        '.join(self.data['keywords']))
+        if ('project-url' in self.data and self.data['project-url']):
+            fp.write(u'project-url:\n')
+            for line in self.data['project-url']:
+                fp.write(u'    %s\n' % line)
 
-        for name in ('Home-page', 'author', 'author_email',
-                     'maintainer', 'maintainer_email', 'description-file'):
+        for name in ('keywords', 'platform', 'supported-platform', 'classifier',
+                     'requires-dist', 'provides-dist', 'obsoletes-dist', 'requires-externals'):
             if (name in self.data and self.data[name]):
-               fp.write(u'%s = %s\n' % (name.decode('utf-8'),
-                                        self.data[name].decode('utf-8')))
+                fp.write(u'%s = ' % name)
+                fp.write(self.multi_string(self.data[name]))
 
-        if ('description' in self.data and self.data['description']):
-            fp.write(
-                     u'description = %s\n'
-                     % u'\n       |'.join(self.data['description'].split('\n')))
+        if ('requires-python' in self.data and self.data['requires-python']):
+            fp.write(u'requires-python = %s\n' % self.data['requires-python'])
+        fp.write(u'\n')
 
-        # multiple use string entries
-        for name in ('platforms', 'supported-platform', 'classifiers',
-                     'requires-dist', 'provides-dist', 'obsoletes-dist', 'requires-external'):
-            if not(name in self.data and self.data[name]):
-                continue
-            fp.write(u'%s = ' % name)
-            fp.write(u''.join('    %s\n' % val
-                              for val in self.data[name]).lstrip())
+        fp.write(u'[files]\n')
 
-        fp.write(u'\n[files]\n')
-
-        for name in ('packages', 'py_modules', 'scripts', 'extra_files', 'data_files'):
-            if not(name in self.data and self.data[name]):
-                continue
-            fp.write(u'%s = %s\n'
-                     % (name, u'\n    '.join(self.data[name]).strip()))
+        for name in ('packages', 'modules', 'scripts'):
+            if (name in self.data and self.data[name]):
+                fp.write(u'%s = %s\n'
+                         % (name, u'\n    '.join(self.data[name]).strip()))
 
         if self.data.get('package_data'):
             fp.write(u'package_data =\n')
@@ -332,16 +245,38 @@ class CreateSetup(object):
                 fp.write(u'    %s = %s\n' % (pkg, spec))
             fp.write(u'\n')
 
+        if ('data_files' in self.data and self.data['data_files']):
+            fp.write(u'data_files = [\n')
+            for dir, filematch in sorted(self.data['data_files'].items()):
+                fp.write(u"    ('%s', %s),\n" % (dir, filematch))
+            fp.write(u']\n\n')
+
         if self.data.get('resources'):
             fp.write(u'resources =\n')
             #for src, dest in self.data['resources']:
-                #fp.write(u'    %s = %s\n' % (src, dest))
+            #fp.write(u'    %s = %s\n' % (src, dest))
             for entry in self.data['resources']:
                 fp.write(u'    %s\n' % entry)                
             fp.write(u'\n')
 
         fp.close()
 
+    # make absolute sure that this file exists?
+    def convert_gramps_sh(self):
+        gramps_sh_in   = 'gramps.sh.in'
+        gramps_sh_file = 'gramps.sh'
+        if (os.path.exists(gramps_sh_in) and not os.path.exists(gramps_sh_file)):
+            shutil.copy(gramps_sh_in, gramps_sh_file)
+
+    # make absolute sure that this file exists?
+    def convert_const_file(self):
+        const_py_in   = os.path.join('gramps', 'const.py.in')
+        const_py_file = os.path.join('gramps', 'const.py')
+        if (os.path.exists(const_py_in) and not os.path.exists(const_py_file)):
+            shutil.copy(const_py_in, const_py_file)
+
 if __name__ == "__main__":
     cs = CreateSetup()
     cs.main()
+    cs.convert_gramps_sh()
+    cs.convert_const_file()
